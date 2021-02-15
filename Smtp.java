867c89b97087311a59f9c6797191d53152be624f
vasu <vasu.gmd@gamil.com>
2021-02-15 12:48:55 +0530

    update Smtp.java

---
+++ Smtp.java
@@ -0,0 +1,17 @@
+
+Home@Hello MINGW64 /f/Practice/Git-Gitub/Sample_Repository (master)
+$ Dbconnection.java  download.java    Ftpcon.java  StringExample.java
+decryption.java    encryption.java  Mail.java    Upload.java
+
+Home@Hello MINGW64 /f/Practice/Git-Gitub/Sample_Repository (master)
+$
+
+
+
+
+
+
+
+
+
+
